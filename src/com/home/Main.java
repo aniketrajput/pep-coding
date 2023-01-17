a6181ca5fd01a744cf0cb9031fd9b9af0fb09b50
aniketrajput <aniketrajput90@gmail.com>
2023-01-17 20:14:36 +0530

    Fibonacci program

--- src/com/home/Main.java
+++ src/com/home/Main.java
@@ -1,5 +1,6 @@
 package com.home;
 
+import com.home.dynammicprogramming.Fibonacci;
 import com.home.recurssion.*;
 
 import java.io.IOException;
@@ -8,6 +9,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        KnightTour.printKnightTour();
+        Fibonacci.findFibonacci();
     }
 }
