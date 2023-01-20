2ac1577049be8348eb5145e50632b6fee904b6ac
aniketrajput <aniketrajput90@gmail.com>
2023-01-20 18:25:43 +0530

    Climbing stairs with minumum moves program

--- src/com/home/Main.java
+++ src/com/home/Main.java
@@ -2,6 +2,7 @@ package com.home;
 
 import com.home.dynammicprogramming.ClimbingStairs;
 import com.home.dynammicprogramming.ClimbingStairsVariableJumps;
+import com.home.dynammicprogramming.ClimbingStairsWithMinimumMoves;
 import com.home.dynammicprogramming.Fibonacci;
 import com.home.recurssion.*;
 
@@ -11,6 +12,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        ClimbingStairsVariableJumps.climbingStairsCount();
+        ClimbingStairsWithMinimumMoves.findMimMoves();
     }
 }
