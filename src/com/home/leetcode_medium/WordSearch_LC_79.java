bc0e51d17519e28a6734784feab772c88aaeb593
Aniket Rajput <aniketrajput90@gmail.com>
2024-07-08 17:18:18 +0530

    new programs

---
+++ src/com/home/leetcode_medium/WordSearch_LC_79.java
@@ -0,0 +1,68 @@
+package com.home.leetcode_medium;
+/*
+
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example 1:
+    Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+    Output: true
+
+Example 2:
+    Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+    Output: true
+
+Example 3:
+    Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+    Output: false
+
+Constraints:
+    m == board.length
+            n = board[i].length
+
+    1 <= m, n <= 6
+            1 <= word.length <= 15
+
+    board and word consists of only lowercase and uppercase English letters.
+
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
+*/
+
+
+public class WordSearch_LC_79 {
+    public boolean exist(char[][] board, String word) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, int i, int j, int count, String word) {
+        if (word.length() == count)
+            return true;
+
+        if (i == -1 || i == board.length || j == -1 || j == board[0].length || board[i][j] != word.charAt(count)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '*';
+
+        boolean found = dfs(board, i + 1, j, count + 1, word) ||
+                dfs(board, i - 1, j, count + 1, word) ||
+                dfs(board, i, j + 1, count + 1, word) ||
+                dfs(board, i, j - 1, count + 1, word) ;
+
+        board[i][j] = temp;
+
+        return found;
+    }
+}
